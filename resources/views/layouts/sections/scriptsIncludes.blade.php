4825aebc11e989ecf035bcf6d8b2db9caf56b7f3
habimana Alexis <97063294+Alexisadmin@users.noreply.github.com>
2023-04-03 09:37:52 +0200

    add cars info

--- resources/views/layouts/sections/scriptsIncludes.blade.php
+++ resources/views/layouts/sections/scriptsIncludes.blade.php
@@ -4,9 +4,6 @@
 <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
 <script src="{{ asset('assets/js/config.js') }}"></script>
 
-<!-- beautify ignore:end -->
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
 <script async="async" src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
